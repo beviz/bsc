e3ebaf29d0fb4bb5da44fba0ed4953b472c6c3ce
bevis <i@bevis.me>
2019-01-08 19:38:31 +0800

    支持强制覆盖安装

--- install.sh
+++ install.sh
@@ -8,18 +8,23 @@ else
   DEV=false
 fi
 
+if [ "$FORCE" = true ]; then
+  FORCE=true
+else
+  FORCE=false
+fi
+
 ln_confirm() {
-  if [ -e $2 ]; then
+  if [[ -e $2 && "$FORCE" = false ]]; then
     read -p "$2 exists, overwrite? (Y/n): " -n 1 -r
     echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      rm -rf $2
-    else
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
       echo "skipped: $2"
       return 1
     fi
   fi
 
+  rm -rf $2
   ln -s $BASEDIR/$1 $2
   echo "added: $2"
 }
